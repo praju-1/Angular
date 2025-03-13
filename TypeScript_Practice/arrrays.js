4261cad55fbd52632ff28f25f8c925b0b5f26dcd
Praju-1 <prajaktatayde26@gmail.com>
2025-03-13 16:45:06 +0530

    Arrays in typescript

---
+++ TypeScript_Practice/arrrays.js
@@ -0,0 +1,21 @@
+// let fruits : string[] = ["Apple", "Banana", "Orange"]
+// console.log(fruits);
+// let sweets : Array<string> = ["Apple", "Banana", "Orange"]
+// console.log(sweets);
+// var values : (string|number)[] = [2, 3, 4, "dfadf", "fdaf"]
+// console.log(values);
+// // accessing elements from your index
+// console.log(values[0]);
+// for(var index in fruits){
+//     console.log(fruits[index]);
+// }
+// // another syntax
+// for(var i=0; i<values.length; i++){
+//     console.log(values[i]);
+// }
+// let numbers : number[] = [22, 34, 66, 78, 89];
+// let data : number = numbers.push(8)
+// console.log(numbers);
+// console.log(data);
+var arr2 = ["hello", 234, true, false, [1, 2, 3, 4]];
+console.log(typeof arr2);
