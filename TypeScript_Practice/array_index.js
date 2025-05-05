bda5623beaaeae15dee715c2c024ea55f6835e90
Praju-1 <prajaktatayde26@gmail.com>
2025-05-05 11:05:17 +0530

    Array index

---
+++ TypeScript_Practice/array_index.js
@@ -0,0 +1,3 @@
+var items = [1, 2, 3];
+console.log(items === null || items === void 0 ? void 0 : items[0]);
+console.log(items === null || items === void 0 ? void 0 : items[10]);
---
+++ TypeScript_Practice/array_index.ts
@@ -0,0 +1,4 @@
+let items = [1, 2,3]
+
+console.log(items ?.[0]);
+console.log(items ?.[10]);
