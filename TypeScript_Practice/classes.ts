2eea199399bcc75b69175e25ba459a592e3cf5ce
Praju-1 <prajaktatayde26@gmail.com>
2025-06-06 18:19:38 +0530

    create a class for typescript

--- TypeScript_Practice/classes.ts
+++ TypeScript_Practice/classes.ts
@@ -1,22 +1,22 @@
-// class Person{
-//     name : string;
-//     age : number;
+class Person{
+    name : string;
+    age : number;
 
-//     constructor(name:string, age:number) {
-//         this.name = name
-//         this.age = age
-//     }
+    constructor(name:string, age:number) {
+        this.name = name
+        this.age = age
+    }
 
-//     greet():void{
-//         console.log(`Hello my name is ${this.name} and I am ${this.age} years old.`);
+    greet():void{
+        console.log(`Hello my name is ${this.name} and I am ${this.age} years old.`);
         
-//     }
-// }
+    }
+}
 
-// const info = new Person("John", 23)
-// info.greet()
-// console.log(info.age);
-// console.log(info.name);
+const info = new Person("John", 23)
+info.greet()
+console.log(info.age);
+console.log(info.name);
 
 
 class Employee{
@@ -37,5 +37,3 @@ const emp = new Employee("john", 30000)
 console.log(emp.name);
 console.log(emp.getSalary());
 // console.log(emp.salary);
-
-
