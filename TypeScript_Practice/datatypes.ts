d558526cc4db7a0adecd15fd405933fbd177c01e
Praju-1 <prajaktatayde26@gmail.com>
2025-06-22 20:52:08 +0530

    Data types in typescript

---
+++ TypeScript_Practice/datatypes.ts
@@ -0,0 +1,26 @@
+// let var_name : datatype = value 
+let value : boolean = false
+console.log(value);
+console.log(typeof value);
+
+let num : number = 10
+console.log(num);
+console.log(typeof num);
+
+let names : string = "john"
+console.log(names);
+console.log(typeof names);
+
+let y : undefined = undefined
+console.log(typeof y);
+
+
+let a : any = 68.3
+console.log(typeof a);
+console.log(Math.round(a));
+
+let z
+console.log(z);
+
+let x : null = null
+console.log(x);
